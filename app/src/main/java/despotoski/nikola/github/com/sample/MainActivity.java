ee42405680e2c34825628be6e50949e002ac1ad0
Nikola <nikola.despotoski@gmail.com>
2016-03-24 21:47:01 +0100

    WTF

--- app/src/main/java/despotoski/nikola/github/com/sample/MainActivity.java
+++ app/src/main/java/despotoski/nikola/github/com/sample/MainActivity.java
@@ -7,45 +7,46 @@ import android.util.Log;
 import despotoski.nikola.github.com.bottomnavigationlayout.BottomNavigationItem;
 import despotoski.nikola.github.com.bottomnavigationlayout.BottomNavigationItemBuilder;
 import despotoski.nikola.github.com.bottomnavigationlayout.BottomTabLayout;
+import despotoski.nikola.github.com.bottomnavigationlayout.sample.R;
 
 public class MainActivity extends AppCompatActivity implements BottomTabLayout.OnNavigationItemSelectionListener {
     private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(despotoski.nikola.github.com.bottomnavigationlayout.R.layout.activity_main);
-        BottomTabLayout tabLayout = (BottomTabLayout) findViewById(despotoski.nikola.github.com.bottomnavigationlayout.R.id.tabs);
+        setContentView(R.layout.activity_main);
+        BottomTabLayout tabLayout = (BottomTabLayout) findViewById(R.id.tabs);
         tabLayout.setOnNavigationItemSelectionListener(this);
         tabLayout.setShiftingMode(true);
         BottomTabLayout.BottomTabsBuilder builder = new BottomTabLayout.BottomTabsBuilder();
         builder.addBottomNavigationItem(
                 new BottomNavigationItemBuilder()
                         .setText("Text 1")
-                        .setIcon(despotoski.nikola.github.com.bottomnavigationlayout.R.drawable.ic_android_white_24dp)
+                        .setIcon(R.drawable.ic_android_white_24dp)
                         .setParentColorBackground(android.R.color.holo_blue_bright)
                         .build());
 
         builder.addBottomNavigationItem(
                 new BottomNavigationItemBuilder()
                         .setText("Text 2")
-                        .setIcon(despotoski.nikola.github.com.bottomnavigationlayout.R.drawable.ic_favorite_white_24dp)
+                        .setIcon(R.drawable.ic_favorite_white_24dp)
                         .setParentColorBackground(android.R.color.holo_orange_light)
                         .build());
         builder.addBottomNavigationItem(
                 new BottomNavigationItemBuilder().setText("Text 3")
-                        .setIcon(despotoski.nikola.github.com.bottomnavigationlayout.R.drawable.ic_favorite_white_24dp)
+                        .setIcon(R.drawable.ic_favorite_white_24dp)
                         .setParentColorBackground(android.R.color.holo_blue_dark)
                         .build());
         builder.addBottomNavigationItem(
                 new BottomNavigationItemBuilder()
                         .setText("Text 4")
-                        .setIcon(despotoski.nikola.github.com.bottomnavigationlayout.R.drawable.ic_place_white_24dp)
+                        .setIcon(R.drawable.ic_place_white_24dp)
                         .setParentColorBackground(android.R.color.holo_blue_bright)
                         .build());
         builder.addBottomNavigationItem(
                 new BottomNavigationItemBuilder()
                         .setText("Text 5")
-                        .setIcon(despotoski.nikola.github.com.bottomnavigationlayout.R.drawable.ic_favorite_white_24dp)
+                        .setIcon(R.drawable.ic_favorite_white_24dp)
                         .setParentColorBackground(android.R.color.holo_green_light)
                         .build());
         tabLayout.populateBottomTabItems(builder);
